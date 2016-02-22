f3452efae6f1f7dfc6eea741807e49896acc7274
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2016-02-23 00:30:42 +0100

    Now... the initial tests

---
+++ RefactoringExample1/test/ecos/org/CustomerStatementTests.java
@@ -0,0 +1,55 @@
+package ecos.org;
+
+import org.ecos.Customer;
+import org.ecos.Movie;
+import org.ecos.Rental;
+import org.junit.Test;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+
+public class CustomerStatementTests {
+    @Test
+    public void statementForASingleRegularMovieAndASingleDay(){
+        Customer customer = new Customer("Martin");
+        String movieName = "The Wizard of Oz";
+        Movie movie = new Movie(movieName,Movie.REGULAR);
+        Rental rental = new Rental(movie,1);
+        customer.addRentals(rental);
+        String result = customer.statement();
+
+        assertThat(result, containsString(String.format("%s\t%s",movieName,2.0)));
+        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s","Amounts owed is",2.0)));
+    }
+
+    @Test
+    public void statementForASingleChildrensMovieAndASingleDay(){
+        Customer customer = new Customer("Martin");
+        String movieName = "Teen beach movie";
+        Movie movie = new Movie(movieName,Movie.CHILDRENS);
+        Rental rental = new Rental(movie,1);
+        customer.addRentals(rental);
+        String result = customer.statement();
+
+        assertThat(result, containsString(String.format("%s\t%s",movieName,1.5)));
+        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s","Amounts owed is",1.5)));
+    }
+
+    @Test
+    public void statementForASingleNewReleaseMovieAndASingleDay(){
+        Customer customer = new Customer("Martin");
+        String movieName = "Teen beach movie";
+        Movie movie = new Movie(movieName,Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie,1);
+        customer.addRentals(rental);
+        String result = customer.statement();
+
+        assertThat(result, containsString(String.format("%s\t%s",movieName,3)));
+        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s","Amounts owed is",3)));
+    }
+
+}
