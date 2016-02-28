04d18c823d5e450a117e7d2f2676de2b0d9c9dac
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2016-02-28 13:15:58 +0100

    Starting the test refactoring   + Apply the object mother to all the tests   + Specifying the different parts of a tests

--- RefactoringExample1/test/ecos/org/CustomerStatementTests.java
+++ RefactoringExample1/test/ecos/org/CustomerStatementTests.java
@@ -3,7 +3,6 @@ package ecos.org;
 import ecos.org.objectMother.CustomerObjectMother;
 import org.ecos.Customer;
 import org.ecos.Movie;
-import org.ecos.Rental;
 import org.junit.Test;
 
 import static org.hamcrest.core.StringContains.containsString;
@@ -11,50 +10,58 @@ import static org.junit.Assert.assertThat;
 
 public class CustomerStatementTests {
     @Test
-    public void statementForASingleRegularMovieAndASingleDay(){
+    public void statementForASingleRegularMovieAndASingleDay() {
         //Arrange
         String movieName = "The Wizard of Oz";
+        int daysRented = 1;
 
         Customer customer = CustomerObjectMother.
             initializeTo("Martin").
-                addRental(1,movieName,Movie.REGULAR).
+                addRental(daysRented, movieName, Movie.REGULAR).
             generate();
 
         //Act
         String result = customer.statement();
 
         //Asserts
-        assertThat(result, containsString(String.format("%s\t%s",movieName,2.0)));
-        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
-        assertThat(result, containsString(String.format("%s %s","Amounts owed is",2.0)));
+        assertThat(result, containsString(String.format("%s\t%s", movieName, 2.0)));
+        assertThat(result, containsString(String.format("%s %s %s", "You earned", 1, "frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s", "Amounts owed is", 2.0)));
     }
 
     @Test
-    public void statementForASingleChildren9MovieAndASingleDay(){
-        Customer customer = new Customer("Martin");
+    public void statementForASingleChildren9MovieAndASingleDay() {
+        //Arrange
         String movieName = "Teen beach movie";
-        Movie movie = new Movie(movieName,Movie.CHILDRENS);
-        Rental rental = new Rental(movie,1);
-        customer.addRentals(rental);
+        int daysRented = 1;
+        Customer customer = CustomerObjectMother.
+            initializeTo("Martin").
+                addRental(daysRented, movieName, Movie.CHILDRENS).
+            generate();
+
+        //Act
         String result = customer.statement();
 
-        assertThat(result, containsString(String.format("%s\t%s",movieName,1.5)));
-        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
-        assertThat(result, containsString(String.format("%s %s","Amounts owed is",1.5)));
+        assertThat(result, containsString(String.format("%s\t%s", movieName, 1.5)));
+        assertThat(result, containsString(String.format("%s %s %s", "You earned", 1, "frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s", "Amounts owed is", 1.5)));
     }
 
     @Test
-    public void statementForASingleNewReleaseMovieAndASingleDay(){
-        Customer customer = new Customer("Martin");
-        String movieName = "Teen beach movie";
-        Movie movie = new Movie(movieName,Movie.NEW_RELEASE);
-        Rental rental = new Rental(movie,1);
-        customer.addRentals(rental);
+    public void statementForASingleNewReleaseMovieAndASingleDay() {
+        //Arrange
+        String movieName = "Inside out";
+        int daysRented = 1;
+        Customer customer = CustomerObjectMother.
+            initializeTo("Martin").
+                addRental(daysRented,movieName,Movie.NEW_RELEASE).
+            generate();
+
+        //Act
         String result = customer.statement();
 
-        assertThat(result, containsString(String.format("%s\t%s",movieName,3)));
-        assertThat(result, containsString(String.format("%s %s %s","You earned",1,"frequent renter points")));
-        assertThat(result, containsString(String.format("%s %s","Amounts owed is",3)));
+        assertThat(result, containsString(String.format("%s\t%s", movieName, 3)));
+        assertThat(result, containsString(String.format("%s %s %s", "You earned", 1, "frequent renter points")));
+        assertThat(result, containsString(String.format("%s %s", "Amounts owed is", 3)));
     }
-
 }
